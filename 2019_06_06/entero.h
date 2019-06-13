eb156df3d39bb370db490f75b564187cf3dc670c
davidurregomartinez <51010172+davidurregomartinez@users.noreply.github.com>
2019-06-13 15:00:06 -0500

    Add files via upload
    archivos de clase

---
+++ 2019_06_06/entero.h
@@ -0,0 +1,13 @@
+#ifndef __entero__
+#define __entero__
+
+#define entero int
+#define real float
+#define suma(a,b) a + b
+#define resta(a,b) a - b
+#define multiplicacion(a,b) a * b
+#define division(a,b) a / b
+
+
+#endif
+
